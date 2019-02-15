83b07b8ed273935fd40534cad04b55c8af18ae1b
kevinegstorf <kevin.egstorf@gmail.com>
2019-02-16 00:58:25 +0100

    first vue project

---
+++ video-browser/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <SearchBar @termChange="onTermChange"></SearchBar>
+    </div>
+</template>
+
+<script>
+import SearchBar from './components/SearchBar';
+
+export default {
+    name: 'App',
+    components: {
+        SearchBar
+    },
+    methods: {
+        onTermChange(searchTerm) {
+            console.log('searchTerm :', searchTerm);
+        }
+    }
+};
+</script>
---
+++ video-browser/src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <input @input="onInput" type="text">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SearchBar',
+        methods: {
+            onInput: function(event){
+               this.$emit('termChange', event.target.value);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
+
